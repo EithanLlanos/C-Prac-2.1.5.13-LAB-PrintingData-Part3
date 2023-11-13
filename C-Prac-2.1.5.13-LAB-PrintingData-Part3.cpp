94cfa097135f8ddcc1ee2c208729e41fe8690292
EithanLlanos <eithanllanosb@gmail.com>
2023-11-13 12:15:15 -0500

    first commit

---
+++ C-Prac-2.1.5.13-LAB-PrintingData-Part3.cpp
@@ -0,0 +1,36 @@
+//Scenario
+//Write a small program which prints a figure like the one in the Expected Output section below.
+//Your version of the program must print the same result as the expected output.Remember, if you want to print the \ character, then you have to escape it, like this: \\.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Expected output
+//       ^      
+//     /   \     
+//   /       \   
+//  <         > 
+//   \       /   
+//     \   /     
+//       v       
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//#include <stdio.h>
+//
+//int main()
+//{
+//	/* your code */
+//	return 0;
+//}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+int main() {
+	printf("       ^ \n");
+	printf("     /   \\ \n");
+	printf("   /       \\ \n");
+	printf(" <           > \n");
+	printf("   \\       / \n");
+	printf("     \\   / \n");
+	printf("       v \n");
+	return 0;
+}
